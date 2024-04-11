54d7a4a1cbc73f23fd9030b316e07227b049c9cd
Thejas Kiran P S <thejaskiranps@gmail.com>
2024-04-12 00:28:43 +0530

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import vector from "./images/vector.png";
 import "./globals.css";
-import { Stack } from "@mantine/core";
+import { Button, Stack } from "@mantine/core";
 import Header from "./header";
 import { useRouter } from 'next/navigation'
 
@@ -12,23 +12,26 @@ export default function Page() {
   return (
     <Stack>
       <Header />
-   <main>
-    <div className={styles.mainn}>
-      <div>
-        <div className={styles.map}><Image src={vector} alt="map" width={550} height={550}/></div>
-      </div>
-      <div className={styles.mainbox}>
-        <div className={styles.tagline1}><h6 className={styles.connect}>Connect everything.</h6></div>
-        <div className={styles.tagline}>
-          <h6 className={styles.build}>Build</h6>
-          <h6 className={styles.anything}>anything.</h6>
+      <main>
+        <div className={styles.mainn}>
+          <div>
+            <div className={styles.map}><Image src={vector} alt="map" width={550} height={550} /></div>
+          </div>
+          <div className={styles.mainbox}>
+            <div className={styles.tagline1}><h6 className={styles.connect}>Connect everything.</h6></div>
+            <div className={styles.tagline}>
+              <h6 className={styles.build}>Build</h6>
+              <h6 className={styles.anything}>anything.</h6>
+            </div>
+              <Button
+                onClick={() => router.push('/sign-in')}
+                size={'lg'}
+              >
+                Get Started
+              </Button>
+          </div>
         </div>
-        
-        <div><button type="button" className={styles.b1} onClick={() => router.push('/page1')}>Get Started</button></div>
-        
-      </div>
-   </div>
-   </main>
-   </Stack>
+      </main>
+    </Stack>
   );
 }
