8ded52f2fef419cf3618e3773c6e673a66c3b586
Thejas Kiran P S <thejaskiranps@gmail.com>
2024-04-23 23:51:17 +0530

    fix colorScheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript
           defaultColorScheme="dark"
+          forceColorScheme='dark'
         />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
