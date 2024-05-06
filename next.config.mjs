66b50585a7791ab983ad1b4b61169d06dbd2cb08
Thejas Kiran P S <thejaskiranps@gmail.com>
2024-05-07 01:01:59 +0530

    fix type errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
